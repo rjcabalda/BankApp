f14d2dcfebf39f57c1abd94d8ab3c04172c2aa5e
Rio Joy Cabalda COMMSEC <rWX979427@china.huawei.com>
2021-01-20 16:17:49 +0800

    Add load table function

--- javascript.js
+++ javascript.js
@@ -5,15 +5,21 @@ function toggleMenu() { //onclick listener on toggle button(.toggle)
     navigation.classList.toggle('active');
     toggle.classList.toggle('active');
 }
-let users = [];
+
+let users = JSON.parse(localStorage.getItem('users'));
+if (users === null) {
+    users = [];
+}
+loadTable();
+
 
 function create_user(user, balance) {
     let userObj = {
         name: user,
-        amount: balance
+        amount: parseInt(balance)
     }
     users.push(userObj);
-    console.log(users);
+    localStorage.setItem('users', JSON.stringify(users));
 }
 
 let createForm = document.querySelector('.form');
@@ -22,19 +28,39 @@ let inputs = document.querySelectorAll('.text');
 
 
 createBtn.addEventListener('click', () => {
-    var elements = document.getElementById("form").elements;
-    var user = {};
-    for (let i = 0; i < elements.length; i++) {
-        var item = elements.item(i);
-        user[item.name] = item.value;
+    
+    if (createForm.checkValidity()) {
+        var elements = createForm.elements;
+        var user = {};
+        for (let i = 0; i < elements.length; i++) {
+            var item = elements.item(i);
+            user[item.name] = item.value;
+        }
+        user.fullname = function () {
+            return this.firstname + ' ' + this.lastname;
+        }
+        create_user(user.fullname(), user.amount);
+        for (const input of inputs) {
+            input.value = '';
+        }
+        loadTable();
+    } else {
+        alert('please fill required field(s).');
 
     }
-    for (const input of inputs) {
-        // input.innerHTML = '';
-        console.log(input);
-    }
-    user.fullname = function () {
-        return this.firstname + ' ' + this.lastname;
-    }
-    create_user(user.fullname(), user.amount);
 });
+function loadTable() {
+    let tbody = document.getElementById('tbody');
+    tbody.innerHTML = "";
+
+    for (let i = 0; i < users.length; i++) {
+        let tr = "<tr>";
+        let full_name = users[i].name;
+        let amount = users[i].amount;
+        tr += "<td>" + (i + 1) + "</td>" + "<td>" + full_name + "</td>" + "<td>₱ " + parseFloat(amount) + "</td></tr>";
+        tbody.innerHTML += tr;
+    }
+}
+
+
+
