fb592c62cdfb8d37e119b7431199da4a8233e2a2
Rio Joy Cabalda COMMSEC <rWX979427@china.huawei.com>
2021-01-22 12:54:21 +0800

    Deposit function

--- javascript.js
+++ javascript.js
@@ -3,44 +3,39 @@
 let createForm = document.querySelector('.form');
 let createBtn = document.getElementById('createBtn');
 let inputs = document.querySelectorAll('.text');
-let users = JSON.parse(localStorage.getItem('users'));
 let side_btn = document.querySelectorAll('.side_btn');
 let dashboard = document.querySelector('.dashboard');
 let create = document.querySelector('.createAccount');
-// let deposit = document.getElementById('deposit');
-// let withdraw = document.getElementById('withdraw');
-// let transfer = document.getElementById('transfer');
-// let users = [];
+let users = JSON.parse(localStorage.getItem('users'));
 
-for (const btn of side_btn) {
+// for (const btn of side_btn) {
+//     btn.addEventListener('click', (e) => {
+//         console.log(btn.id);
+//         switch (btn.id) {
+//             case 'dashboard':
+//                 loadTable();
+//                 break;
+//             case 'create':
+//                 create.classList.remove('hide');
+//                 dashboard.classList.add('hide');
+//                 break;
+//             case 'deposit':
+//                 break;
+//             case 'withdraw':
+//                 break;
+//             case 'transfer':
+//                 break;
+//         }
 
-    console.log(btn);
-    btn.addEventListener('click', (e) => {
-        console.log(btn.id);
-        switch (btn.id) {
-            case 'dashboard':
-                dashboard.classList.remove('hide');
-                create.classList.add('hide');
-                break;
-            case 'create':
-                create.classList.remove('hide');
-                dashboard.classList.add('hide');
-                break;
-            case 'deposit':
-                break;
-            case 'withdraw':
-                break;
-            case 'transfer':
-                break;
-        }
-
-    });
-}
-
+//     });
+// }
+window.addEventListener('load', (event) => {
+    loadTable();
+});
 if (users === null) {
     users = [];
 }
-if (loadTable());
+
 
 
 function toggleMenu() { //onclick listener on toggle button(.toggle)
@@ -56,7 +51,7 @@ function create_user(user, balance) {
     }
     let userStatus = true;
     for (const fullname of users) {
-        if (fullname.name === user) userStatus = false;
+        if (fullname.name.toLowerCase() === user.toLowerCase()) userStatus = false;
     }
     if (userStatus) {
         users.push(userObj);
@@ -68,23 +63,25 @@ function create_user(user, balance) {
 }
 function loadTable() {
     let tbody = document.getElementById('tbody');
-    tbody.innerHTML = "";
-    if (parseInt(users.length) === 0) tbody.innerHTML = "</tr><td class='noData'colspan='3'>No Data</td></tr>";
+    if (tbody !== null) {
+        tbody.innerHTML = "";
+        if (parseInt(users.length) === 0) tbody.innerHTML = "</tr><td class='noData'colspan='3'>No Data</td></tr>";
 
-    for (let i = 0; i < users.length; i++) {
-        let tr = "<tr>";
-        let full_name = users[i].name;
-        let amount = users[i].amount;
-        let balance = 0;
+        for (let i = 0; i < users.length; i++) {
+            let tr = "<tr>";
+            let full_name = users[i].name;
+            let amount = users[i].amount;
+            let balance = 0;
 
-        if (parseInt(amount) > 0) balance = amount.toFixed(2);
-        tr += "<td class='col1'>" + (i + 1) + "</td>" + "<td class='col2'>" + full_name + "</td>" + "<td class='col3'>₱ " + balance + "</td></tr>";
+            if (parseInt(amount) > 0) balance = parseFloat(amount).toFixed(2);
+            tr += "<td class='col1'>" + (i + 1) + "</td>" + "<td class='col2'>" + full_name + "</td>" + "<td class='col3'>₱ " + balance + "</td></tr>";
 
-        tbody.innerHTML += tr;
+            tbody.innerHTML += tr;
+        }
     }
 }
 
-createBtn.addEventListener('click', () => {
+createBtn?.addEventListener('click', () => {
 
     if (createForm.checkValidity()) {
         var elements = createForm.elements;
@@ -103,10 +100,11 @@ createBtn.addEventListener('click', () => {
         }
         loadTable();
     } else {
-        alert('please fill required field(s).');
+        // alert('please fill required field(s).');
 
     }
 });
+
 
 
 
